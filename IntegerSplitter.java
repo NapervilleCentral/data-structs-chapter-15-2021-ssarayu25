e1fcf075a38b1058fa2b8f8fbf36dd3012e47430
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2022-09-01 11:46:04 -0500

    integer splitter

--- IntegerSplitter.java
+++ IntegerSplitter.java
@@ -14,11 +14,19 @@ public class IntegerSplitter
    {
       // Complete this method. Use a Stack
       Stack<Integer> q = new Stack<>();
-      
-
-
-
-
+     
+      Integer x = new Integer(number);
+      while (x > 0)
+      {
+          Integer temp = new Integer(x%10);
+          q.add(temp);
+          x = x/10;
+    }
+    while (q.size() > 0)
+    {
+        System.out.print(q.pop() + " ");
+    }
+    
 
 
    }
